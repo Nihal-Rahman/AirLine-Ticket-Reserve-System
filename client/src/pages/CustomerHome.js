dc80c7bed8eeaa6250d9adb83a520d3ccb1088e6
Tanvi <tanvi.rahman@nyu.edu>
2023-05-01 21:52:33 -0400

    adding nihal customerhome.js

--- client/src/pages/CustomerHome.js
+++ client/src/pages/CustomerHome.js
@@ -1,77 +1,60 @@
 import React from 'react';
 import axios from "axios";
-import { useEffect, useState } from "react"; 
-import CustomerNavbar from '../components/CustomerNavbar'
+import { useEffect, useState } from "react";
 
-function CustomerHome(){
+function CustomerHome() {
 
-    const [listOfFlights, setListOfFlights] = useState([]);
+  const [listOfFlights, setListOfFlights] = useState([]);
 
-    /*
-    const getUserFlights = () => {
-        axios.get("localhost:3001/customer/viewFlights", {
-            headers: {
-                accessToken: sessionStorage.getItem("accessToken")
-            }
-        }).then((response)=>{
-            if(response.data.error){
-                alert("You are not logged in!");
-            } else{}
-        })
-    }
-    */
+  useEffect(() => {
+    axios.get("http://localhost:3001/customer/viewFlights",
+      {
+        headers: {
+          accessToken: sessionStorage.getItem("accessToken"),
+        },
+      }
+    ).then((response) => {
+      if (response.data.error) {
+        alert("You are not logged in!");
+      } else {
+        setListOfFlights(response.data);
+      }
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get("http://localhost:3001/customer/viewFlights",
-          {
-            headers: {
-              accessToken: sessionStorage.getItem("accessToken"),
-            },
-          }
-        ).then((response) => {
-          if (response.data.error) {
-            alert("You are not logged in!");
-          } else {
-            setListOfFlights(response.data);
-          }
-        });
-      }, []);
-
-    return(
-      <div>
-        <CustomerNavbar />
-          <section>
-              <h3>Your Upcoming Flights:</h3>
-        <table className="table">
-          <thead>
+  return (
+    <section>
+      <div>Your Upcoming Flights:</div>
+      <table className="table">
+        <thead>
+          <tr>
             <th>Ticket ID</th>
-            <th>Airline Name</th>
             <th>Flight Num</th>
             <th>Departure Date</th>
             <th>Departure Time</th>
             <th>Airline Name</th>
             <th>First Name</th>
             <th>Last Name</th>
-          </thead>
-          <tbody>
-            {listOfFlights.map((value,key) => {
-              return ( 
-                <tr> 
-                  <td> {value.ticket_ID} </td> 
-                  <td> {value.flight_num} </td> 
-                  <td> {value.departure_date} </td> 
-                  <td> {value.departure_time} </td>
-                  <td> {value.airline_name} </td>
-                  <td> {value.first_name} </td> 
-                  <td> {value.last_name} </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-          </section>
-      </div>
-    );
+          </tr>
+        </thead>
+        <tbody>
+          {listOfFlights.map((value, key) => {
+            return (
+              <tr>
+                <td> {value.ticket_ID} </td>
+                <td> {value.flight_num} </td>
+                <td> {value.departure_date} </td>
+                <td> {value.departure_time} </td>
+                <td> {value.airline_name} </td>
+                <td> {value.first_name} </td>
+                <td> {value.last_name} </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </section>
+  );
 }
 
 export default CustomerHome
