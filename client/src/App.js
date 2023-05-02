d5bd97f9d1c728413930c66e5572d7fec40416d6
Tanvi <tanvi.rahman@nyu.edu>
2023-05-01 22:01:10 -0400

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,35 @@
 import './App.css';
-import{BrowserRouter as Router, Route, Routes, Link} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from './pages/Home';
 import Register from './pages/Register';
 import StaffRegister from './pages/StaffRegister';
 import StaffLogin from './pages/StaffLogin';
 import Navbar from './components/Navbar';
 import Login from './pages/Login'
 import CustomerHome from './pages/CustomerHome';
-import StaffHome from './pages/StaffHome';
-import CreateFlight from './pages/StaffCases/CreateFlight';
-import ViewFlights from './pages/StaffCases/ViewFlights'
-import AddPlane from './pages/StaffCases/AddPlane'
-import AddAirport from './pages/StaffCases/AddAirport'
-import Flight from './pages/StaffCases/Flight';
-import Home from './pages/Home';
+import CancelFlights from './pages/CancelFlights';
+import SearchFlights from './pages/SearchFlights';
 
 function App() {
   return <div className="App">
-    <Router> 
+    <Navbar relative flex absolute inset-y-0 />
+    <Router>
       <Routes>
-        <Route path='/' exact element={<Home/>}/>
-        <Route path='/staff/register' exact element={<StaffRegister/>}/>
-        <Route path='/staff/login' exact element={<StaffLogin/>}/>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/staff/register' exact element={<StaffRegister />} />
+        <Route path='/staff/login' exact element={<StaffLogin />} />
         <Route path='/user/register' exact element={<Register />} />
         <Route path='/user/login' exact element={<Login />} />
         <Route path='/customer/home' exact element={<CustomerHome />} />
+        <Route path='/customer/cancelFlights' exact element={<CancelFlights />} />
+        <Route path='/customer/search' exact element={<SearchFlights />} />
         <Route path='/staff/home' exact element={<StaffHome />} />
         <Route path='/staff/create-flight' exact element={<CreateFlight />} />
         <Route path='/staff/add-plane' exact element={<AddPlane />} />
         <Route path='/staff/add-airport' exact element={<AddAirport />} />
-        
-        <Route path='/flight/:flight_num/:departure_date/:departure_time' exact element={<Flight />} />
       </Routes>
     </Router>
-    </div>;
+  </div>;
 }
 
 export default App;
