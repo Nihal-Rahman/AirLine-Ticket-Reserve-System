83c637bc88ddb707d1b2c76d0c80a8a8ac07b8fb
Tanvi <tanvi.rahman@nyu.edu>
2023-05-05 12:26:00 -0400

    final

--- server/connection.js
+++ server/connection.js
@@ -3,8 +3,8 @@ const mySQL = require('mysql2');
 const db = mySQL.createConnection({
     host: "localhost",
     user: "root",
-    password: "nihal1234",
-    database: "DBproject"
+    password: "",
+    database: "projectDB"
 });
 
 db.connect((err) => {
